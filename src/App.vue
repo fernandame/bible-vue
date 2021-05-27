202995a5f91390222bd9225735c31bb12e94b8d0
Fernanda Mesquita <fernandamexquita@gmail.com>
2021-05-26 22:38:12 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Bible />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "tailwindcss/tailwind.css";
+import Bible from "@/components/Bible.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Bible },
+};
 </script>
 
 <style>
+html,
+body,
+#app {
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Bible.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="p-16">
+    <p class="text-4xl" v-if="isLoading === false">
+      {{ bookName }}, {{ currentChapter }}
+    </p>
+    <ul class="mt-6">
+      <li
+        class="text-left odd:bg-gray-100 p-2"
+        v-for="(verse, i) in verses"
+        :key="i"
+      >
+        {{ i + 1 }}. {{ verse.text }}
+      </li>
+    </ul>
+    <div class="flex justify-center space-x-5 mt-6">
+      <button
+        class="p-3 bg-green-500 text-white shadow-md disabled:opacity-60"
+        :disabled="(bookIndex === 0 && currentChapter <= 1) || isLoading"
+        @click="fetchPrev()"
+      >
+        Anterior
+      </button>
+      <button
+        class="p-3 bg-green-500 text-white shadow-md disabled:opacity-60"
+        :disabled="isLoading"
+        @click="fetchNext()"
+      >
+        Próximo
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+const token = process.env.VUE_APP_API_KEY;
+
+const api = axios.create({
+  baseURL: "https://www.abibliadigital.com.br/api/",
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+export default {
+  name: "Bible",
+  data() {
+    return {
+      books: [],
+      bookIndex: 0,
+      currentChapter: 1,
+      bookName: "",
+      verses: [],
+      isLoading: true,
+    };
+  },
+  async mounted() {
+    await this.fetchBooks();
+    await this.getData();
+  },
+  methods: {
+    async fetchBooks() {
+      const res = await api.get("books");
+      this.books = res.data;
+    },
+
+    async getData() {
+      this.verses = [];
+
+      const book = this.books[this.bookIndex];
+
+      const res = await api
+        .get(`verses/nvi/${book.abbrev.pt}/${this.currentChapter}`)
+        .catch((err) => {
+          console.log(err);
+        });
+
+      this.isLoading = false;
+      this.bookName = res.data.book.name;
+      this.verses = res.data.verses;
+    },
+
+    fetchNext() {
+      this.isLoading = true;
+      this.currentChapter++;
+
+      let book = this.books[this.bookIndex];
+
+      const isFinalChapter = this.currentChapter + 1 > book.chapters;
+
+      if (isFinalChapter) {
+        this.bookIndex++;
+        book = this.books[this.bookIndex];
+        this.currentChapter = 1;
+      }
+
+      this.getData();
+    },
+
+    fetchPrev() {
+      this.isLoading = true;
+      this.currentChapter--;
+
+      let book = this.books[this.bookIndex];
+
+      if (this.currentChapter < 1) {
+        this.bookIndex--;
+        book = this.books[this.bookIndex];
+        this.currentChapter = book.chapters;
+      }
+
+      this.getData();
+    },
+  },
+};
+</script>
+
+<style></style>
